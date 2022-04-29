2970264c80e4e112f6062520d5a1bfabfdfab620
Sylwester Czmil <sylwekczmil@gmail.com>
2022-04-29 10:14:20 +0200

    Release 0.2.0

--- cacp/__init__.py
+++ cacp/__init__.py
@@ -1,9 +1,3 @@
-"""Top-level package for cacp."""
-
-__author__ = """Sylwester Czmil"""
-__email__ = 'sylwekczmil@gmail.com'
-__version__ = '0.1.4'
-
 from cacp.dataset import \
     ClassificationDatasetBase, \
     ClassificationDataset, \
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,4 +1,9 @@
-from cacp import all_datasets
+from pathlib import Path
+
+import numpy as np
+
+from cacp import all_datasets, LocalClassificationDataset
+from cacp_examples.example_custom_datasets.random_dataset import RandomDataset
 
 
 def test_dataset_all():
@@ -34,3 +39,33 @@ def test_dataset_2(datasets):
     assert ds.features == 8
     assert ds.classes == 2
     assert ds.instances == 768
+
+
+def test_custom_dataset():
+    ds = RandomDataset()
+    assert ds.name == 'RandomDataset'
+    assert ds.features == 5
+    assert ds.classes == 2
+    assert ds.instances == 100
+
+    for i, fold in enumerate(ds.folds()):
+        assert fold.index == i
+        assert np.all(fold.labels == np.array([0, 1]))
+        assert fold.x_train.shape == (90, 5)
+        assert fold.y_train.shape == (90,)
+        assert fold.x_test.shape == (10, 5)
+        assert fold.y_test.shape == (10,)
+
+
+def test_local_dataset():
+    ds_path = Path(__file__).parent.parent. \
+        joinpath('cacp_examples') \
+        .joinpath('example_custom_datasets') \
+        .joinpath('local_dataset')
+    ds = LocalClassificationDataset('iris', ds_path)
+    assert ds.name == 'iris'
+    assert ds.output_name == 'Class'
+    assert ds.origin == 'Real world'
+    assert ds.features == 4
+    assert ds.classes == 3
+    assert ds.instances == 150
